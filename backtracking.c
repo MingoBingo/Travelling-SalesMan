cc43b57df100848d287eed44da74664ce8c435d5
Andrei Florea <andrei.florea2207@stud.acs.upb.ro>
2026-08-14 15:36:24 +0300

    Changed from 4 corners to only entrance and exit

--- backtracking.c
+++ backtracking.c
@@ -9,13 +9,21 @@ const int cornerPermutations[24][4] = {
     {3, 1, 0, 2}, {3, 1, 2, 0}, {3, 2, 0, 1}, {3, 2, 1, 0}
 };
 
+
+const int inOutPermutations[12][2] = {
+    {0, 1}, {0, 2}, {0, 3},
+    {1, 0}, {1, 2}, {1, 3},
+    {2, 0}, {2, 1}, {2, 3},
+    {3, 0}, {3, 1}, {3, 2}
+};
+
 int solutionBKT(int numberOfRooms, int roomsCompleted)
 {
     return (numberOfRooms == roomsCompleted);
 }
 
 void findPathBkt(Point dockingStation, Point currentStep, float currentCost, float *minTotalCost, int roomsCompleted, Room *room, 
-                int numberOfRooms, Point *currentPath, Point *bestPath, int *algorithmPlace, float *bktHistory)
+                int numberOfRooms, Point *currentPath, Point *bestPath, int *algorithmPlace, float *bktHistory, float **costInteriorMatrix)
 {
     if(currentCost >= (*minTotalCost))
         return;
@@ -45,21 +53,30 @@ void findPathBkt(Point dockingStation, Point currentStep, float currentCost, flo
         if(!room[i].visited)
         {
             room[i].visited = 1;
-             for(int j = 0; j < 24; ++j)
+             for(int j = 0; j < 12; ++j)
             {
                 float currentCostBranch = currentCost;
-                currentCostBranch += distance(currentStep, room[i].corner[cornerPermutations[j][0]]);
-                currentCostBranch += distance(room[i].corner[cornerPermutations[j][0]], room[i].corner[cornerPermutations[j][1]]);
-                currentCostBranch += distance(room[i].corner[cornerPermutations[j][1]], room[i].corner[cornerPermutations[j][2]]);
-                currentCostBranch += distance(room[i].corner[cornerPermutations[j][2]], room[i].corner[cornerPermutations[j][3]]);
+                /*
+                    currentCostBranch += distance(currentStep, room[i].corner[cornerPermutations[j][0]]);
+                    currentCostBranch += distance(room[i].corner[cornerPermutations[j][0]], room[i].corner[cornerPermutations[j][1]]);
+                    currentCostBranch += distance(room[i].corner[cornerPermutations[j][1]], room[i].corner[cornerPermutations[j][2]]);
+                    currentCostBranch += distance(room[i].corner[cornerPermutations[j][2]], room[i].corner[cornerPermutations[j][3]]);
 
-                currentPath[roomsCompleted * 4] = room[i].corner[cornerPermutations[j][0]];
-                currentPath[roomsCompleted * 4 + 1] = room[i].corner[cornerPermutations[j][1]];
-                currentPath[roomsCompleted * 4 + 2] = room[i].corner[cornerPermutations[j][2]];
-                currentPath[roomsCompleted * 4 + 3] = room[i].corner[cornerPermutations[j][3]];
+                    currentPath[roomsCompleted * 4] = room[i].corner[cornerPermutations[j][0]];
+                    currentPath[roomsCompleted * 4 + 1] = room[i].corner[cornerPermutations[j][1]];
+                    currentPath[roomsCompleted * 4 + 2] = room[i].corner[cornerPermutations[j][2]];
+                    currentPath[roomsCompleted * 4 + 3] = room[i].corner[cornerPermutations[j][3]];
+                Code using all four corners of every room*/
+                
+                currentCostBranch += distance(currentStep, room[i].corner[inOutPermutations[j][0]]);
+                currentCostBranch += costInteriorMatrix[i][j];
 
-                findPathBkt(dockingStation, room[i].corner[cornerPermutations[j][3]], currentCostBranch, minTotalCost, roomsCompleted + 1, room, numberOfRooms, 
-                            currentPath, bestPath, algorithmPlace, bktHistory);
+                currentPath[roomsCompleted * 2] = room[i].corner[inOutPermutations[j][0]];
+                currentPath[roomsCompleted * 2 + 1] = room[i].corner[inOutPermutations[j][1]];
+
+                findPathBkt(dockingStation, room[i].corner[inOutPermutations[j][1]], currentCostBranch, minTotalCost, roomsCompleted + 1, room, numberOfRooms, 
+                            currentPath, bestPath, algorithmPlace, bktHistory, costInteriorMatrix);
+                
             }
             room[i].visited = 0;
         }
--- backtracking.h
+++ backtracking.h
@@ -6,10 +6,11 @@
 #include "geometry.h"
 
 extern const int cornerPermutations[24][4];
+extern const int inOutPermutations[12][2];
 
 int solutionBKT(int numberOfRooms, int roomsCompleted);
 
 void findPathBkt(Point dockingStation, Point currentStep, float currentCost, float *minTotalCost, int roomsCompleted, Room *room, 
-                int numberOfRooms, Point *currentPath, Point *bestPath, int *algorithmPlace, float *bktHistory);
+                int numberOfRooms, Point *currentPath, Point *bestPath, int *algorithmPlace, float *bktHistory, float **costInteriorMatrix);
 
 #endif
